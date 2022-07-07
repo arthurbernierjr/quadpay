4d78dd4e60c84af246a04af82be3f2b1119438f2
Arthur Bernier Jr <ma2bernier@gmail.com>
2022-07-07 17:45:27 -0400

    Minor Changes
    Set Up to Deploy to Cloud

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const PORT = 8088
+const path = require('path')
+
+app.use(express.static('build'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(PORT, () => console.log('no issues'))
